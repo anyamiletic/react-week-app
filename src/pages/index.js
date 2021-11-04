dc78ac2e294d3be5548c634e7ebbf2cccb061340
anyamiletic <anya.miletic@gmail.com>
2021-11-04 10:28:46 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import NextLink from 'next/link'
 
-export default function Home() {
+import styles from 'styles/Home.module.css'
+
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +16,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <NextLink href="/about">About</NextLink>
         </h1>
 
         <p className={styles.description}>
@@ -67,3 +70,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
